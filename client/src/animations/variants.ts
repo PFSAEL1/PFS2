936e015e5b0c1cd004d9982f7d0c2ee2048bad42
PFS <isaac@pfsspraybooths.com>
2026-08-11 18:49:15 +0530

    Fixed animation issue

--- client/src/animations/variants.ts
+++ client/src/animations/variants.ts
@@ -13,32 +13,26 @@ export const fadeIn = {
   },
 };
 
+const isMobile = window.innerWidth <= 420;
+
 export const slideLeft = {
   hidden: {
     opacity: 0,
-    transform: "translateX(-100px)",
+    transform: `translateX(${isMobile ? -60 : -100}px)`,
   },
   visible: {
     opacity: 1,
     transform: "translateX(0px)",
-    transition: {
-      duration: 0.2,
-      ease: "easeOut",
-    },
   },
 };
 
 export const slideRight = {
   hidden: {
     opacity: 0,
-    transform: "translateX(100px)",
+    transform: `translateX(${isMobile ? 60 : 100}px)`,
   },
   visible: {
     opacity: 1,
     transform: "translateX(0px)",
-    transition: {
-      duration: 0.8,
-      ease: "easeOut",
-    },
   },
 };
